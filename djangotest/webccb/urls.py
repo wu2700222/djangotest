aa79240da43651bf0dab96c838458edd378c1b46
wuwei <wu2700222@126.com>
2019-04-08 16:41:50 +0800

    11

--- djangotest/webccb/urls.py
+++ djangotest/webccb/urls.py
@@ -3,13 +3,13 @@ from . import view,search_get,search_post
 
 urlpatterns=[
     #http://127.0.0.1:8080/hello
-    url('hello', view.hello),
+    url(r'^hello$', view.hello),
     #http://127.0.0.1:8080/get
     #模拟get参数
-    url('get', search_get.search_form),
+    url(r'^get$', search_get.search_form),
     #http://127.0.0.1:8080/search_get/?q=wuwei   
     #get请求
-    url('^search$', search_get.get),
+    url(r'^search$', search_get.get),
     #post请求 ^ 与 $分别为匹配开始与结束符,精确匹配
     url(r'^search-post$', search_post.post),
 
